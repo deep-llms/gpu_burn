ebeadbcf8029be714b76d4ac8f00da66c0545616
Thien Nguyen <deep-llms>
2026-06-25 20:49:57 -0700

    update

--- commands.sh
+++ commands.sh
@@ -1,9 +1,7 @@
-#2
-#nvidia-smi
-nvidia-smi
-
+#1
 #gpu-burn
+
 #sudo apt-get update
 #sudo apt-get install -y cuda-toolkit-13-0
-#make CUDAPATH=/usr/local/cuda-13.0
-#./gpu_burn 36000000000
+make CUDAPATH=/usr/local/cuda-13.0
+./gpu_burn 36000000000
